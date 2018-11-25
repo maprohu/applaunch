9cbb1ceb699797bb5b597cf23e4453cbc4108cc9
Marton Papp <maprohu@gmail.com>
2018-11-25 17:43:11 +0000

    resources

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,18 @@
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "kotlin2js") {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+            }
+        }
+    }
+}
+
 include(
-        "builder"
+        "builder",
+        "libs/common",
+        "libs/firebase",
+        "apps/testapp",
+        "apps/gymclock",
+        "apps/stock"
 )
